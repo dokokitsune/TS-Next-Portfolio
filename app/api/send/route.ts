20d2cc9238df9c860cc4842b99583c5afac3504f
Weston Wood <stakin101@gmail.com>
2024-03-10 14:19:48 -0700

    Contact me form email works

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,22 +1,40 @@
 import nodemailer from "nodemailer";
+import * as aws from '@aws-sdk/client-ses'
+
+const ses = new aws.SES({
+    apiVersion: "2012-10-17",
+    region: "us-west-2",
+    credentials: {
+        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
+
+    }
+    
+})
+
 
 const transporter = nodemailer.createTransport({
-    auth:{
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PWD,
-        
-    }
+    SES: {ses, aws}
 })
 
 
 export function sendEmail(emailTemplate : string){
-    var message = {
-        from: process.env.EMAIL_USER,
-        to: process.env.RECV_EMAIL,
-        subject: "Portfolio Contact Response",
-        html: emailTemplate,
-    }
+    
 
-    transporter.sendMail(message);
+    transporter.sendMail(
+        {
+            from: 'contact@wwoodportfolio.awsapps.com',
+            to: 'wwood98@outlook.com',
+            subject: "Test Email",
+            html: emailTemplate,
+        },
+        (error, info) => {
+            if(error){
+                console.error(error)
+            } else{
+                console.log('Email sent:', info.messageId)
+            }
+        }
+    );
 
 }
